68e01b73b048911ed9a68bc62878ca501ea41a56
Ben Parsell <parsellb@mail.gvsu.edu>
2023-04-05 18:43:27 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
+
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
